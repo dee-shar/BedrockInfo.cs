66c46885a84030ddfaafaa1b2c14b6d094217164
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 01:55:07 +0300

    Update BedrockInfo.cs

--- src/BedrockInfo.cs
+++ src/BedrockInfo.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
 namespace BedrockInfoApi
 {
@@ -9,7 +8,8 @@ namespace BedrockInfoApi
     {
         private readonly HttpClient httpClient;
         private readonly string apiUrl = "https://api.bedrockinfo.com/v2";
-        public BedrockInfo() {
+        public BedrockInfo()
+        {
             httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
@@ -17,9 +17,9 @@ namespace BedrockInfoApi
                 new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<string> GetServerStatus(string address, int port = 19132, int history = 5) {
+        public async Task<string> GetServerStatus(string address, int port = 19132, int history = 5)
+        {
             var response = await httpClient.GetAsync($"{apiUrl}/status?server={address}&port={port}&history={history}");
-            response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
     }
